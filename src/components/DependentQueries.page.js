ebb727d9a0fb98628f7431e7650d924984f5121f
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-01-08 10:30:12 +0630

    Dependent Queries

---
+++ src/components/DependentQueries.page.js
@@ -0,0 +1,29 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchUserByEmail = (email) => {
+  return axios.get(`http://localhost:4000/users/${email}`);
+};
+
+const fetchCoursesByChannelId = (channelId) => {
+  return axios.get(`http://localhost:4000/channels/${channelId}`);
+};
+
+export const DependentQueriesPage = ({ email }) => {
+  const { data: user } = useQuery(["user", email], () =>
+    fetchUserByEmail(email)
+  );
+  const channelId = user?.data.channelId;
+
+  const { data: channel } = useQuery(
+    ["courses", channelId],
+    () => fetchCoursesByChannelId(channelId),
+    {
+      enabled: !!channelId,
+    }
+  );
+  console.log({ channelId });
+  console.log({ channel });
+
+  return <div>DependentQueries.page</div>;
+};
