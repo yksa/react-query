2178777a82d041de53e8025a6d3c02acd6f8079f
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-01-12 14:31:24 +0630

    Infinite Queries

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { ParallelQueriesPage } from "./components/ParallelQueries.page";
 import { DynamicParallelPage } from "./components/DynamicParallel.page";
 import { DependentQueriesPage } from "./components/DependentQueries.page";
 import { PaginatedQueriesPage } from "./components/PaginatedQueries.page";
+import { InfiniteQueriesPage } from "./components/InfiniteQueries.page";
 
 const queryClient = new QueryClient();
 
@@ -32,6 +33,9 @@ function App() {
             </ul>
           </nav>
           <Switch>
+            <Route path="/rq-infinite">
+              <InfiniteQueriesPage />
+            </Route>
             <Route path="/rq-paginated">
               <PaginatedQueriesPage />
             </Route>
