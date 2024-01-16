95298a043ddc93e6dd2eb65f3dcf576a137e6884
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-01-16 10:58:41 +0630

    Axios Interceptor

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { useQuery, useMutation, useQueryClient } from "react-query";
+import { request } from "../utils/axios-utils";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  // return axios.get("http://localhost:4000/superheroes");
+  return request({ url: "/superheroesd" });
 };
 
 const addSuperHero = (hero) => {
-  return axios.post("http://localhost:4000/superheroes", hero);
+  // return axios.post("http://localhost:4000/superheroes", hero);
+  return request({ url: "/superheroes", method: "post", data: hero });
 };
 
 export const useSuperHeroesData = (onSuccess, onError) => {
